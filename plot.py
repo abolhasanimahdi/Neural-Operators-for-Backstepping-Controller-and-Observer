0fad2c0387c873699fd48c212cd59e1dc9b70478
Mahdi Abolhasani <mahdiabolhasani@live.com>
2026-01-03 09:24:47 +0330

    Support multiple PDE

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Plot PDE solution u(x,t)
-def plot_pde(x, t, u):
+def plot_pde(x, t, u, title):
     X, T = np.meshgrid(x, t)
     fig = plt.figure(figsize=(7, 5))
     ax = fig.add_subplot(111, projection='3d')
@@ -10,12 +10,12 @@ def plot_pde(x, t, u):
     ax.set_xlabel('x')
     ax.set_ylabel('t')
     ax.set_zlabel('u(x,t)')
-    ax.set_title('PDE solution u(x,t)')
+    ax.set_title(title)
     plt.tight_layout()
-    plt.show()
+    plt.show(block=False)
 
 # Plot kernel solution k(x,y)
-def plot_kernel(x, k):
+def plot_kernel(x, k, title):
     X, Y = np.meshgrid(x, x)
     fig = plt.figure(figsize=(7, 5))
     ax = fig.add_subplot(111, projection='3d')
@@ -23,6 +23,6 @@ def plot_kernel(x, k):
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('k(x,y)')
-    ax.set_title('Kernel solution k(x,y)')
+    ax.set_title(title)
     plt.tight_layout()
-    plt.show()
+    plt.show(block=False)
