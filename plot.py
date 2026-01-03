07f454cee0eb7414eafa64da21c6118ab51f6e33
Mahdi Abolhasani <mahdiabolhasani@live.com>
2026-01-03 14:49:46 +0330

    Highlight U(t)

--- plot.py
+++ plot.py
@@ -7,9 +7,13 @@ def plot_pde(x, t, u, title):
     fig = plt.figure(figsize=(7, 5))
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_surface(T, X, u)
+    x1_line = np.ones_like(t) * x[-1]
+    ax.plot(t, x1_line, u[:, -1], zorder=10)
     ax.set_xlabel('t')
     ax.set_ylabel('x')
     ax.set_zlabel('u(x,t)')
+    ax.set_xlim(1.5, 0)
+    ax.view_init(elev=10, azim=100)
     ax.set_title(title)
     plt.tight_layout()
     plt.show(block=False)
@@ -24,7 +28,7 @@ def plot_kernel(x, k, title):
     ax.set_ylabel('x')
     ax.set_zlabel('k(x,y)')
     ax.set_title(title)
-    ax.set_ylim(1, 0)  # reverse x-axis
+    ax.set_ylim(1, 0)
     plt.tight_layout()
     plt.show(block=False)
 
@@ -36,6 +40,17 @@ def plot_lambda(x, lambda_func, title):
     plt.plot(x, y)
     plt.xlabel('x')
     plt.ylabel(r'$\lambda(x)$')
+    plt.title(title)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show(block=False)
+
+# Plot controller U(t)
+def plot_controller(t, U, title):
+    plt.figure(figsize=(7, 5))
+    plt.plot(t, U)
+    plt.xlabel('t')
+    plt.ylabel('U(t)')
     plt.title(title)
     plt.grid(True)
     plt.tight_layout()
