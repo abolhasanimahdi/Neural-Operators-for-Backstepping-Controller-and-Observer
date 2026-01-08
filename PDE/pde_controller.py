bbcd1e22875d451db4c07c1bf6b59b86eef6052b
Mahdi Abolhasani <mahdiabolhasani@live.com>
2026-01-08 11:56:16 +0330

    Solve initial value problem for U(t)

--- PDE/pde_controller.py
+++ PDE/pde_controller.py
@@ -8,13 +8,15 @@ def solve_kernel(pde):
     lam = pde.lambda_func(x)
     k = np.zeros((Nx, Nx))
 
-    for i in range(1, Nx - 1):
+    for i in range(0, Nx - 1):  # Start from 0 to fill k[1,1]
+        k[i + 1, i + 1] = k[i, i] - (h / 4.0) * (lam[i] + lam[i + 1])
+        k[i + 1, i] = k[i, i] - (h / 4.0) * lam[i]
         k[i + 1, 0] = 0
-        k[i + 1, i + 1] = k[i, i] - h / 4 * (lam[i - 1] + lam[i])
-        k[i + 1, i] = k[i, i] - h / 2 * lam[i]
 
         for j in range(1, i):
-            k[i + 1, j] = -k[i - 1, j] + k[i, j + 1] + k[i, j - 1] + lam[j] * h ** 2 * (k[i, j + 1] + k[i, j - 1]) / 2
+            term_reaction = (lam[j] * h ** 2) * (k[i, j + 1] + k[i, j - 1]) / 2.0
+            k[i + 1, j] = k[i, j + 1] + k[i, j - 1] - k[i - 1, j] + term_reaction
+
     return k
 
 def compute_control(k, current_u, h):
