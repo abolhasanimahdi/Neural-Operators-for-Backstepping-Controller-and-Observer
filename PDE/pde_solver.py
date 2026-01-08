386369e2c48b56fee36b16505b10e71cc02bcc5d
Mahdi Abolhasani <mahdiabolhasani@live.com>
2026-01-08 16:10:24 +0330

    Add observer to the solver

--- PDE/pde_solver.py
+++ PDE/pde_solver.py
@@ -1,26 +1,40 @@
-# pde_solver.py
 import numpy as np
 from PDE.pde_controller import compute_control
 
-def solve_pde(pde, k=None):
+def solve_pde(pde, k_ctrl=None, k_obs=None):
     Nx, Nt = pde.Nx, pde.Nt
     h, dt = pde.h, pde.dt
     x = pde.x
     lam = pde.lambda_func(x)
     u = np.zeros((Nt, Nx))
+    u_hat = np.zeros((Nt, Nx))
     u[0, :] = pde.ux0_func(x)
-    if k is not None:
-        u0_control = compute_control(k, u[0, :], h)
-        u[0, -1] = u0_control
-    else:
-        u[0, -1] = 0
+    u_hat[0, :] = 0
+
     for n in range(Nt - 1):
-        diffusion = (u[n, 2:] - 2 * u[n, 1:-1] + u[n, :-2]) / h ** 2
-        reaction = lam[1:-1] * u[n, 1:-1]
-        u[n + 1, 1:-1] = u[n, 1:-1] + dt * (diffusion + reaction)
-        u[n + 1, 0] = pde.u0t_func((n + 1) * dt)
-        if k is not None:
-            u[n + 1, -1] = compute_control(k, u[n + 1, :], h)
+        if k_ctrl is not None:
+            state_for_control = u_hat[n, :] if k_obs is not None else u[n, :]
+            U_t = compute_control(k_ctrl, state_for_control, h)
         else:
-            u[n + 1, -1] = pde.u1t_func((n + 1) * dt)
-    return u
+            U_t = pde.u1t_func(n * dt)
+        diff_u = (u[n, 2:] - 2 * u[n, 1:-1] + u[n, :-2]) / h ** 2
+        react_u = lam[1:-1] * u[n, 1:-1]
+        u[n + 1, 1:-1] = u[n, 1:-1] + dt * (diff_u + react_u)
+
+        u[n + 1, 0] = pde.u0t_func((n + 1) * dt)
+        u[n + 1, -1] = U_t
+
+        if k_obs is not None:
+            ux_1 = (u[n, -1] - u[n, -2]) / h
+            u_hat_x_1 = (u_hat[n, -1] - u_hat[n, -2]) / h
+            meas_error = ux_1 - u_hat_x_1
+
+            diff_hat = (u_hat[n, 2:] - 2 * u_hat[n, 1:-1] + u_hat[n, :-2]) / h ** 2
+            react_hat = lam[1:-1] * u_hat[n, 1:-1]
+            gain_term = k_obs[1:-1] * meas_error
+
+            u_hat[n + 1, 1:-1] = u_hat[n, 1:-1] + dt * (diff_hat + react_hat + gain_term)
+            u_hat[n + 1, 0] = 0
+            u_hat[n + 1, -1] = U_t
+
+    return u, u_hat
--- main.py
+++ main.py
@@ -3,23 +3,37 @@ import numpy as np
 from PDE.pde_model import PDE
 from PDE.pde_solver import solve_pde
 from PDE.pde_controller import solve_kernel
-from plot import plot_pde, plot_kernel, plot_lambda
+from plot import plot_pde, plot_kernel, plot_lambda, plot_error_norm
 import matplotlib.pyplot as plt
 
 # Define PDE
 def lambda_func2(x): return 50 * np.cos(8 * np.arccos(x))
 pde2 = PDE(lambda_func=lambda_func2)
 t_grid2 = np.linspace(0, pde2.T, pde2.Nt)
+
+def lambda_func3(x): return 20 * np.cos(5 * np.arccos(x))
+def u1t_func3(t): return 10 * np.cos(np.pi * 2 * t) + 7 * np.sin(16 * t)
+pde3 = PDE(lambda_func=lambda_func3, u1t_func=u1t_func3)
+t_grid3 = np.linspace(0, pde3.T, pde3.Nt)
 # Solve PDE u(x,t)
-u2 = solve_pde(pde2)
+u2, _ = solve_pde(pde2, k_ctrl=None, k_obs=None)
+u3, _ = solve_pde(pde3, k_ctrl=None, k_obs=None)
 # Compute kernel k(x,y)
 k2 = solve_kernel(pde2)
+k3 = solve_kernel(pde3)
 # Solve controlled PDE u(x,t)
-u2_cl = solve_pde(pde2, k2)
+u2_cl, _ = solve_pde(pde2, k_ctrl=k2, k_obs=None)
+u3_cl, _ = solve_pde(pde3, k_ctrl=k3, k_obs=None)
 
+# _, u2_hat = solve_pde(pde2, k_ctrl=None, k_obs=k2[-1, :])
+_, u3_hat = solve_pde(pde3, k_ctrl=None, k_obs=k3[-1, :])
+
+error = u3 - u3_hat
 # Plot results
-plot_pde(pde2.x, t_grid2, u2, title=r'PDE solution $u(x,t)$ for $\lambda(x)=50\cos\!\left(8\cos^{-1}(x)\right)$')
+plot_pde(pde3.x, t_grid3, u3, title=r'PDE solution $u(x,t)$ for $\lambda(x)=50\cos\!\left(8\cos^{-1}(x)\right)$')
 plot_kernel(pde2.x, k2, title=r'Kernel solution $k(x,y)$ for $\lambda(x)=50\cos\!\left(8\cos^{-1}(x)\right)$')
 plot_lambda(pde2.x, lambda_func2, title=r'$\lambda(x)=50\cos\!\left(8\cos^{-1}(x)\right)$')
-plot_pde(pde2.x, t_grid2, u2_cl, title=r'Controlled PDE solution $u(x,t)$ for $\lambda(x)=50\cos\!\left(8\cos^{-1}(x)\right)$')
+plot_pde(pde3.x, t_grid3, u3_cl, title=r'Controlled PDE solution $u(x,t)$ for $\lambda(x)=50\cos\!\left(8\cos^{-1}(x)\right)$')
+plot_pde(pde3.x, t_grid3, u3_hat, title=r'Controlled PDE solution $u(x,t)$ for $\lambda(x)=50\cos\!\left(8\cos^{-1}(x)\right)$')
+plot_error_norm(t_grid3, error)
 plt.show()
