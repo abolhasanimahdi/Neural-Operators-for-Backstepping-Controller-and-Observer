1948a32bf54f37ab5be5a930df2e8de19bb8efe5
Mahdi Abolhasani <mahdiabolhasani@live.com>
2026-01-22 13:51:18 +0330

    Make generate kernel dataset a function

--- DeepONet/generate_data.py
+++ DeepONet/generate_data.py
@@ -13,17 +13,19 @@ def generate_lambda_dataset(amplitude=50, n_samples=1000, nx=101, gamma_min=4, g
         lambdas[i] = amplitude * np.cos(gamma * np.arccos(x))
     return x, lambdas, gammas
 
-x, lambda_dataset, gammas = generate_lambda_dataset(n_samples=6, nx=200)
+def generate_kernel_dataset(lambda_dataset):
+    kernel_dataset = []
+    for i in range(len(lambda_dataset)):
+        lambda_i = lambda_dataset[i]
+        def lambda_func(x, lam=lambda_i):
+            return lam
+        pde = PDE(N=len(lambda_i) - 1, lambda_func=lambda_func)
+        k = solve_kernel(pde)
+        kernel_dataset.append(k)
+    return np.array(kernel_dataset)
 
-kernel_dataset = []
-for i in range(len(lambda_dataset)):
-    lambda_i = lambda_dataset[i]
-    def lambda_func(x, lam=lambda_i):
-        return lam
-    pde = PDE(N=len(lambda_i) - 1, lambda_func=lambda_func)
-    k = solve_kernel(pde)
-    kernel_dataset.append(k)
-kernel_dataset = np.array(kernel_dataset)
+x, lambda_dataset, gammas = generate_lambda_dataset(n_samples=6, nx=200)
+kernel_dataset = generate_kernel_dataset(lambda_dataset)
 
 plot_lambda(x, lambda_dataset, gammas, title=r'$\lambda(x)=50\cos\!\left(\gamma\cos^{-1}(x)\right)$')
 plot_kernel(x, kernel_dataset, title=[rf"Kernel solution $k(x,y)$ for $\lambda(x)=50\cos\!\left({g:.2f}\cos^{{-1}}(x)\right)$" for g in gammas], cols=3)
