b40e4c67b567659d4cc1be4b7025b10913fbb296
Mahdi Abolhasani <mahdiabolhasani@live.com>
2026-01-30 10:42:54 +0330

    Train with paper parameters

--- DeepONet/neural_operator.py
+++ DeepONet/neural_operator.py
@@ -1,10 +1,11 @@
 import numpy as np
 import torch
 import deepxde as dde
+from plot import plot_kernel
 import matplotlib.pyplot as plt
 
-nx = 21
-hidden_size = 16
+nx = 51
+hidden_size = 32
 
 x_grid = np.linspace(0, 1, nx)
 X_mesh, Y_mesh = np.meshgrid(x_grid, x_grid)
@@ -23,21 +24,15 @@ net = dde.nn.DeepONetCartesianProd(
 model = dde.Model(None, net)
 model.compile("adam", lr=1e-3)
 
-checkpoint = torch.load("Operator.pth-3508.pt", map_location=torch.device('cpu'))
+checkpoint = torch.load("Operator.pth-18947.pt", map_location=torch.device('cpu'))
 model.net.load_state_dict(checkpoint["model_state_dict"])
 
 def test_lambda_func(x):
     return 50 * np.cos(8 * np.arccos(x))
 
 lambda_test = test_lambda_func(x_grid).reshape(1, -1).astype(np.float32)
-k_pred_flat = model.predict((lambda_test, trunk_X))
-k_pred = np.tril(k_pred_flat.reshape(nx, nx))
+k_hat_flat = model.predict((lambda_test, trunk_X))
+k_hat = np.tril(k_hat_flat.reshape(nx, nx))
 
-fig = plt.figure(figsize=(10, 7))
-ax = fig.add_subplot(111, projection='3d')
-surf = ax.plot_surface(X_mesh, Y_mesh, k_pred)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('k(x,y)')
-ax.set_title('DeepONet Predicted Kernel $k(x,y)$')
+plot_kernel(x_grid, kernel=k_hat, title=[r'Kernel solution $\hat{k}(x,y)$ for $\lambda(x)=50\cos\!\left(8\cos^{-1}(x)\right)$'], cols=1)
 plt.show()
