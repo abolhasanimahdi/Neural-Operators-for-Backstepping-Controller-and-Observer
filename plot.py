1b78629e1404bf5736b3d0a45f4997cc504adc63
Mahdi Abolhasani <mahdiabolhasani@live.com>
2026-02-04 16:04:08 +0330

    Solve open loop and closed loop PDEs in the notebook

--- plot.py
+++ plot.py
@@ -3,22 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Plot PDE solution u(x,t)
-def plot_pde(x, t, u, title, cols=1):
+def plot_pde(x, t, u, fig_title, title, cols=1):
     if isinstance(u, np.ndarray) and u.ndim == 2:
         u = [u]
     else:
         u = u
     rows = int(np.ceil(len(u) / cols))
     fig = plt.figure(figsize=(cols * 4.5, rows * 3.5))
+    fig.suptitle(fig_title, y=0.9, fontsize=12)
     for idx, current_u in enumerate(u):
         ax = fig.add_subplot(rows, cols, idx + 1, projection='3d')
         X, T = np.meshgrid(x, t)
         ax.plot_surface(T, X, current_u)
         x1_line = np.ones_like(t) * x[-1]
         ax.plot(t, x1_line, current_u[:, -1], zorder=10)
-        ax.set_xlabel('t', fontsize=10, labelpad=-1)
-        ax.set_ylabel('x', fontsize=10, labelpad=-1)
-        ax.set_zlabel('u(x,t)', fontsize=10, labelpad=-5)
+        ax.set_xlabel(r'$t$', fontsize=10, labelpad=-1)
+        ax.set_ylabel(r'$x$', fontsize=10, labelpad=-1)
+        ax.set_zlabel(r'$u(x,t)$', fontsize=10, labelpad=-5)
         ax.xaxis.set_tick_params(labelsize=9, pad=-2)
         ax.yaxis.set_tick_params(labelsize=9, pad=-2)
         ax.zaxis.set_tick_params(labelsize=9, pad=-2)
@@ -28,21 +29,25 @@ def plot_pde(x, t, u, title, cols=1):
     plt.tight_layout()
 
 # Plot kernel solution k(x,y)
-def plot_kernel(x, kernel, title, cols=1):
+def plot_kernel(x, kernel, fig_title, title, cols=1):
     if isinstance(kernel, np.ndarray) and kernel.ndim == 2:
         kernel = [kernel]
     else:
         kernel = kernel
     rows = int(np.ceil(len(kernel) / cols))
-    fig = plt.figure(figsize=(7, 5) if len(kernel) == 1 else (cols * 4.5, rows * 3))
+    fig = plt.figure(figsize=(cols * 4.5, rows * 3.5))
+    fig.suptitle(fig_title, y=0.9, fontsize=12)
     for idx, k in enumerate(kernel):
         ax = fig.add_subplot(rows, cols, idx + 1, projection='3d')
         X, Y = np.meshgrid(x, x)
         ax.plot_surface(Y, X, k)
-        ax.set_xlabel('y')
-        ax.set_ylabel('x')
-        ax.set_zlabel('k(x,y)')
-        ax.set_title(title[idx], y=1, fontsize=12 if len(kernel) == 1 else 10)
+        ax.set_xlabel(r'$y$', fontsize=10, labelpad=-1)
+        ax.set_ylabel(r'$x$', fontsize=10, labelpad=-1)
+        ax.set_zlabel(r'$k(x,y)$', fontsize=10, labelpad=-3)
+        ax.xaxis.set_tick_params(labelsize=9, pad=-2)
+        ax.yaxis.set_tick_params(labelsize=9, pad=-2)
+        ax.zaxis.set_tick_params(labelsize=9, pad=-2)
+        ax.set_title(title[idx], y=0.9, fontsize=10)
         ax.view_init(elev=10, azim=290)
         ax.set_ylim(1, 0)
         ax.set_xlim(0, 1)
@@ -58,7 +63,7 @@ def plot_lambda(x, lambda_func, gamma, title):
     elif lambda_func.ndim == 2:
         for i in range(lambda_func.shape[0]):
             plt.plot(x, lambda_func[i], label=f"γ = {gamma[i]:.2f}")
-    plt.xlabel('x', fontsize=10)
+    plt.xlabel(r'$x$', fontsize=10)
     plt.ylabel(r'$\lambda(x)$', fontsize=10)
     plt.xticks(fontsize=9)
     plt.yticks(fontsize=9)
